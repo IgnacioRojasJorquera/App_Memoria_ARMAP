70954e59bc9ba80ea3718592bd792251475e3e48
NachoRojas <ignaciorojasjoquera@outlook.es>
2021-02-23 00:42:49 -0300

    Se agrega pppup en opciones iniciales
    Se agrega popup en opciones iniciales y arregla texto del popup

---
+++ APP_MEMORIA_ARMAP/Assets/Script/PopupController_opciones.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupController_opciones : MonoBehaviour
+{
+    public GameObject mapa_guia;
+    public GameObject test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Open_MapaGuia()
+    {
+        mapa_guia.SetActive(true);
+    }
+
+    public void Close_MapaGuia()
+    {
+        mapa_guia.SetActive(false);
+    }
+
+    public void Open_Test()
+    {
+        test.SetActive(true);
+    }
+
+    public void Close_Test()
+    {
+        test.SetActive(false);
+    }
+}
