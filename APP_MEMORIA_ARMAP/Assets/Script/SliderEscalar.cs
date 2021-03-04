81ba46388abc7f0a42665648330feddf334e0b03
NachoRojas <ignaciorojasjoquera@outlook.es>
2021-03-04 17:10:02 -0300

    Vetana locacion
    Se agrega ventana para locacion del usuario y otras modificaciones

---
+++ APP_MEMORIA_ARMAP/Assets/Script/SliderEscalar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SliderEscalar : MonoBehaviour
+{
+    public GameObject modelo2d;
+    public Slider sliderEscalar;
+
+    public void EscalarModelo()
+    {
+        modelo2d.transform.localScale = new Vector2(sliderEscalar.value, sliderEscalar.value);
+    }
+}
