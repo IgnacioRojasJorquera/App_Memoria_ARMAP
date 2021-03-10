8d2a1716a700f35235c1e3b309432fd9b449b4cd
NachoRojas <ignaciorojasjoquera@outlook.es>
2021-03-09 23:28:29 -0300

    Se agrega codigo para distancia con json
    Se agrega archivo json con coordenadas de ejemplo y archivo C# para mostrar las coordenadas listadas.
    Arroja error de formato.

---
+++ APP_MEMORIA_ARMAP/Assets/Script/CotroladorGPS.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CotroladorGPS : MonoBehaviour
+{
+    string filePath;
+    string jsonStrig;
+
+    private void Awake()
+    {
+        filePath = Application.dataPath + "/Script/Coordenadas.json";
+        jsonStrig = File.ReadAllText(filePath);
+        ListaCoordenadas listaCoordenadas = JsonUtility.FromJson<ListaCoordenadas>(jsonStrig);
+        //print(listaCoordenadas);
+        listaCoordenadas.Listar();
+    }
+}
+
+[System.Serializable]
+public class Coordenada
+{
+    public float latitud;
+    public float longitud;
+
+    public override string ToString()
+    {
+        return string.Format ("Latitud:{0} , Longitud: {1}", latitud, longitud);
+    }
+}
+
+
+[System.Serializable]
+public class ListaCoordenadas
+{
+    public List<Coordenada> coordenadas;
+
+    public void Listar()
+    {
+        foreach (Coordenada coordenada in coordenadas)
+        {
+            Debug.Log(coordenada);
+        }
+    }
+
+}
