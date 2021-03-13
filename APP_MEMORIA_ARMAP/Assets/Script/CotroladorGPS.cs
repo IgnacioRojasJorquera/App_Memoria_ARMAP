6f38673d19815553b344b05caeadc5f627fe804c
NachoRojas <ignaciorojasjoquera@outlook.es>
2021-03-13 04:18:17 -0300

    Se lista boton prefabs
    -Se hace boton prefab para mostrar los datos de json en modo de lista
    -Se calcula la distancia gps entre coordenada de usuario y la de json

--- APP_MEMORIA_ARMAP/Assets/Script/CotroladorGPS.cs
+++ APP_MEMORIA_ARMAP/Assets/Script/CotroladorGPS.cs
@@ -2,33 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+using System;
 
 public class CotroladorGPS : MonoBehaviour
 {
     string filePath;
     string jsonStrig;
 
+    public GameObject objeto1;
+
+    //private string prefabName = "listPrefab";
     private void Awake()
     {
         filePath = Application.dataPath + "/Coordenadas.json";
         jsonStrig = File.ReadAllText(filePath);
         ListaCoordenadas listaCoordenadas = JsonUtility.FromJson<ListaCoordenadas>(jsonStrig);
         //print(listaCoordenadas);
+       
         listaCoordenadas.Listar();
+
+
+        //Coordenada coordenada = JsonUtility.FromJson<Coordenada>(jsonStrig);
+        //string prefabName; ;
+
+        for (int i =0; i < listaCoordenadas.coordenadas.Count; i++)
+        {
+            GameObject thePrefab = Instantiate(objeto1);
+            //GameObject thePrefab = Instantiate(Resources.Load("Assets/pack2/ButtonPrefabs/listPrefab")) as GameObject;
+            GameObject contentHolder = GameObject.FindWithTag("Content");
+               
+            thePrefab.transform.parent = contentHolder.transform;
+            Text[] theText = thePrefab.GetComponentsInChildren<Text>();
+            theText[0].text = listaCoordenadas.coordenadas[i].nombre;
+
+            Debug.Log(theText[0].text + ": " + Math.Round(GeoCodeCalc.CalcDistance(41.648408, 2.739420, listaCoordenadas.coordenadas[i].latitud, listaCoordenadas.coordenadas[i].longitud, GeoCodeCalcMeasurement.Metre), 2));
+            //Debug.Log(theText[0].text + ": " + Math.Round(GeoCodeCalc.CalcDistance(-18.455141333306248, -70.28160946087549, -18.454795312541773, -70.284366771555, GeoCodeCalcMeasurement.Metre), 2));
+            //double distancia = Math.Round(GeoCodeCalc.CalcDistance(-18.455141333306248, -70.28160946087549, listaCoordenadas.coordenadas[i].latitud, listaCoordenadas.coordenadas[i].longitud, GeoCodeCalcMeasurement.Metre), 2);
+            double distancia = Math.Round(GeoCodeCalc.CalcDistance(GPS.latitude, GPS.longitude, listaCoordenadas.coordenadas[i].latitud, listaCoordenadas.coordenadas[i].longitud, GeoCodeCalcMeasurement.Metre), 2);
+            theText[1].text = "Distancia : " + distancia.ToString() + "m";
+
+            thePrefab.transform.localScale = new Vector3(1,1,1);
+
+            //Button[] button = thePrefab.GetComponentsInChildren<Button>();
+            //button[0].name = listaCoordenadas.coordenadas[i].nombre;
+            /*
+            double tLat = listaCoordenadas.coordenadas[i].latitud;
+            double tLon = listaCoordenadas.coordenadas[i].longitud;
+            string url = "https://www.google.cl/maps/place/Panguipulli+1224,+Arica,+Arica+y+Parinacota/"+tLat+","+tLon+"/@-18.4551617,-70.2837767,17z/data=!3m1!4b1!4m5!3m4!1s0x915aa8f8ba4a38b3:0x847aff94b2ca184b!8m2!3d-18.4551617!4d-70.281588?hl=en&authuser=1";
+            AddListener(button[0], url);
+            */
+        }
     }
+    /*
+    void AddListener(Button b, string url)
+    {
+        b.onClick.AddListener(()=> Application.OpenURL(url));
+    }
+    */
 }
 
 [System.Serializable]
 public class Coordenada
 {
-    public float latitud;
-    public float longitud;
-    
+    public double latitud;
+    public double longitud;
+    public string nombre;
+    /*
     public override string ToString()
     {
-       return string.Format ("Latitud: {0} , Longitud: {1}", latitud, longitud);
+       return string.Format ("Nombre: {0} , Latitud: {1} , Longitud: {2}", nombre, latitud, longitud);
     }
-    
+    */
 }
 
 
@@ -36,12 +82,24 @@ public class Coordenada
 public class ListaCoordenadas
 {
     public List<Coordenada> coordenadas;
-
+    //int i = 0;
     public void Listar()
     {
+        //string theWord = ScenneController;
+        //string prefabName = "listPrefab";
+
+
+
         foreach (Coordenada coordenada in coordenadas)
         {
+            //i++;
             Debug.Log(coordenada);
+            //Debug.Log(i);
+            //GameObject thePrefab = (Resources.Load("ButtonPrefabs/" + prefabName)) as GameObject;
+            //GameObject contentHolder = GameObject.FindGameObjectsWithTag("Content")[0];
+
+            //thePrefab.transform.parent = contentHolder.transform;
+                
         }
     }
 
